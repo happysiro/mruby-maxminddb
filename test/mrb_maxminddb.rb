aaad20a5426212a7e1476060975c922529daee6b
takumakume <takuma.kume@pepabo.com>
2017-08-03 15:38:00 +0900

    fixed test case pattern with no data

--- test/mrb_maxminddb.rb
+++ test/mrb_maxminddb.rb
@@ -3,7 +3,32 @@
 ##
 
 MaxMindDbDat = "/tmp/GeoLite2-City.mmdb"
-IPAddr = '8.8.8.8'
+
+Data1 = {
+  :ip_addr      => "8.8.8.8",
+  :country_code => "US",
+  :region       => nil,
+  :region_name  => nil,
+  :city         => nil,
+  :postal_code  => nil,
+  :latitude     => 37.751000,
+  :longitude    => -97.822000,
+  :metro_code   => nil,
+  :time_zone    => nil,
+}
+
+Data2 = {
+  :ip_addr      => "1.1.1.1",
+  :country_code => "OC",
+  :region       => "VIC",
+  :region_name  => "Victoria",
+  :city         => "Research",
+  :postal_code  => "3095",
+  :latitude     => -37.700000,
+  :longitude    => 145.183300,
+  :metro_code   => nil,
+  :time_zone    => "Australia/Melbourne",
+}
 
 assert("MaxMindDB#new") do
   maxminddb = MaxMindDB.new MaxMindDbDat
@@ -12,67 +37,73 @@ end
 
 assert("MaxMindDB#lookup_string") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 end
 
 assert("MaxMindDB#country_code") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
-  assert_equal("US", maxminddb.country_code)
+  maxminddb.lookup_string Data1[:ip_addr]
+  assert_equal(Data1[:country_code], maxminddb.country_code)
 end
 
 assert("MaxMindDB#region") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal("CA", maxminddb.region)
+  assert_equal(Data1[:region], maxminddb.region)
 end
 
 assert("MaxMindDB#region_name") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal("California", maxminddb.region_name)
+  assert_equal(Data1[:region_name], maxminddb.region_name)
 end
 
 assert("MaxMindDB#city") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal("Mountain View", maxminddb.city)
+  assert_equal(Data1[:city], maxminddb.city) # => nil
+
+  maxminddb.lookup_string Data2[:ip_addr]
+  assert_equal(Data2[:city], maxminddb.city)
 end
 
 assert("MaxMindDB#postal_code") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal("94035", maxminddb.postal_code)
+  assert_equal(Data1[:postal_code], maxminddb.postal_code) # => nil
 end
 
 assert("MaxMindDB#latitude") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal(37.386, maxminddb.latitude)
+  assert_equal(Data1[:latitude], maxminddb.latitude)
 end
 
 assert("MaxMindDB#longitude") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal(-122.0838, maxminddb.longitude.round(4))
+  assert_equal(Data1[:longitude], maxminddb.longitude.round(4))
 end
 
 assert("MaxMindDB#metro_code") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal(807, maxminddb.metro_code)
+  assert_equal(Data1['metro_code'], maxminddb.metro_code)
 end
 
 assert("MaxMindDB#time_zone") do
   maxminddb = MaxMindDB.new MaxMindDbDat
-  maxminddb.lookup_string IPAddr
+  maxminddb.lookup_string Data1[:ip_addr]
 
-  assert_equal("America/Los_Angeles", maxminddb.time_zone)
+  assert_equal(Data1[:time_zone], maxminddb.time_zone) # => nil
+
+  maxminddb.lookup_string Data2[:ip_addr]
+  assert_equal(Data2[:time_zone], maxminddb.time_zone)
 end
